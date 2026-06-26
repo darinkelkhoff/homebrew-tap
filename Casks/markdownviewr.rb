37b56f84a6711975b7bc5cbcfc10fce25a23e664
Darin Kelkhoff <darin.kelkhoff@gmail.com>
2026-06-26 09:00:10 -0500

    markdownViewr: update to v1.4.0

--- Casks/markdownviewr.rb
+++ Casks/markdownviewr.rb
@@ -1,6 +1,6 @@
 cask "markdownviewr" do
-  version "1.3.0"
-  sha256 "0b60e8923ca73d0385d8bb59f1c8afcca52b212b7b5885afee3f89b695da2f33"
+  version "1.4.0"
+  sha256 "75b00e47f673e3bdaeeaba5df4f2c3d9a61f83b971e4c4c5c453439bb5d0231e"
 
   url "https://github.com/darinkelkhoff/markdownViewr/releases/download/v#{version}/markdownViewr.dmg"
   name "markdownViewr"
